a9d80fc774dea40a90e1bfdb41d2fdbfeb0b3a40
techdave <klatzdavie19@gmail.com>
2022-09-30 07:47:23 +0300

    build a todo list

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,21 @@
 "use strict";
-function printDouble(msg) {
-    console.log(msg);
-    console.log(msg);
+var btn = document.getElementById('btn');
+var input = document.getElementById('todoinput');
+var form = document.querySelector('form');
+var list = document.getElementById('todolist');
+function handleSumbit(e) {
+    e.preventDefault();
+    var newTodoText = input.value;
+    var newLi = document.createElement('li');
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    newLi.append(newTodoText);
+    newLi.append(checkbox);
+    list.append(newLi);
+    input.value = '';
 }
-printDouble('HELLO WORLD 😄');
+form.addEventListener('submit', handleSumbit);
+// btn.addEventListener('click', function () {
+//   alert(input.value);
+//   input.value = '';
+// });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-function printDouble(msg: string) {
-  console.log(msg);
-  console.log(msg);
+const btn = document.getElementById('btn')! as HTMLButtonElement;
+const input = document.getElementById('todoinput')! as HTMLInputElement;
+const form = document.querySelector('form')!;
+const list = document.getElementById('todolist')!;
+
+function handleSumbit(e: SubmitEvent) {
+  e.preventDefault();
+  const newTodoText = input.value;
+  const newLi = document.createElement('li');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  newLi.append(newTodoText);
+  newLi.append(checkbox);
+  list.append(newLi);
+  input.value = '';
 }
-printDouble('HELLO WORLD 😄')
+
+form.addEventListener('submit', handleSumbit);
+// btn.addEventListener('click', function () {
+//   alert(input.value);
+//   input.value = '';
+// });
